40b0c188b4bc33f81ef1a4bbb665e1c417c313e0
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-15 20:35:09 +0100

    Add square-root testing code

--- src/main/java/com/github/sqyyy/urban/assembler/Bootstrap.java
+++ src/main/java/com/github/sqyyy/urban/assembler/Bootstrap.java
@@ -6,35 +6,47 @@ import java.nio.file.Path;
 
 public class Bootstrap {
     public static void main(String[] args) throws IOException {
-        var haltModule = new ModuleAssembler();
-        haltModule.nop();
-        haltModule.labelCode("halt:halt");
-        haltModule.halt();
-        var haltModuleC = AssembledModule.compile(haltModule);
+        var math = new ModuleAssembler();
+        math.labelConst("-math:2.0");
+        math.constFloat(2.);
+        math.labelCode("math:sqrt");
+        // X0 = S
+        // X1 = tmp
+        // X2 = xn
+        // X3 = 2
+        math.ldr(3, "-math:2.0");
+        math.divf(2, 0, 3);
+        for (int i = 0; i < 16; i++) {
+            math.divf(1, 0, 2);
+            math.addf(1, 2, 1);
+            math.divf(2, 1, 3);
+        }
+        math.mov(0, 2);
+        math.branch(30);
+        var cMath = AssembledModule.compile(math);
 
         var asm = new ModuleAssembler();
-        asm.addModule(haltModuleC);
-        //        asm.labelConst("&msg");
-        //        asm.constAbsLabel("msg");
-        //        asm.labelConst("msg");
-        //        asm.constCStr("Hello world!\n");
+        asm.addModule(cMath);
+        asm.labelConst("num");
+        asm.constFloat(2.0);
+        asm.labelConst("buf");
+        for (int i = 0; i < 32; i++) {
+            asm.constInt(0x4141414141414141L);
+        }
 
-        asm.nop();
-        asm.nop();
-        asm.nop();
-        asm.branch("halt:halt");
-        asm.nop();
-        asm.nop();
-        asm.nop();
-        asm.constInt(0x8899AABBCCDDEEFFL);
-        //        asm.labelCode("loop");
-        //        asm.mov(0, 2L);
-        //        asm.ldr(1, "&msg");
-        ////        asm.movAbs(1, "msg");
-        //        asm.mov(2, 13L);
-        //        asm.interrupt(1L);
-        //        asm.branch("loop");
-        //        asm.halt();
+        asm.ldr(0, "num");
+        asm.branchLinked("math:sqrt");
+        //        asm.panic();
+
+        asm.movAbs(1, "buf");
+        asm.mov(2, 256L);
+        asm.vcall(0x4202L); // float.toString
+
+        asm.mov(2, 0);
+        asm.mov(0, 1L);
+        asm.movAbs(1, "buf");
+        asm.interrupt(1L); // write
+        asm.halt();
 
         var fio = Files.newOutputStream(Path.of("test.bin"));
         asm.write(fio);
