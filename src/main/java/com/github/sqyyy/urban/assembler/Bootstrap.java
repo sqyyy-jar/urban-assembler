0830b7d860cba1beb6e7a800de55959a1e905141
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-16 12:33:46 +0100

    Add math module

--- src/main/java/com/github/sqyyy/urban/assembler/Bootstrap.java
+++ src/main/java/com/github/sqyyy/urban/assembler/Bootstrap.java
@@ -6,40 +6,43 @@ import java.nio.file.Path;
 
 public class Bootstrap {
     public static void main(String[] args) throws IOException {
-        var math = new ModuleAssembler();
-        math.labelConst("-math:2.0");
-        math.constFloat(2.);
-        math.labelCode("math:sqrt");
-        // X0 = S
-        // X1 = tmp
-        // X2 = xn
-        // X3 = 2
-        math.ldr(3, "-math:2.0");
-        math.divf(2, 0, 3);
-        for (int i = 0; i < 16; i++) {
-            math.divf(1, 0, 2);
-            math.addf(1, 2, 1);
-            math.divf(2, 1, 3);
-        }
-        math.mov(0, 2);
-        math.branch(30);
-        var cMath = AssembledModule.compile(math);
+        // var math = new ModuleAssembler();
+        // math.labelConst("-math:2.0");
+        // math.constFloat(2.);
+        // math.labelCode("math:sqrt");
+        // // X0 = S
+        // // X1 = tmp
+        // // X2 = xn
+        // // X3 = 2
+        // math.ldr(3, "-math:2.0");
+        // math.divf(2, 0, 3);
+        // for (int i = 0; i < 16; i++) {
+        //     math.divf(1, 0, 2);
+        //     math.addf(1, 2, 1);
+        //     math.divf(2, 1, 3);
+        // }
+        // math.mov(0, 2);
+        // math.branch(30);
+        // var cMath = AssembledModule.compile(math);
 
         var asm = new ModuleAssembler();
-        asm.addModule(cMath);
+        asm.addModule(Modules.MATH);
         asm.labelConst("num");
-        asm.constFloat(2.0);
+        asm.constFloat(Double.NaN);
         asm.labelConst("buf");
         for (int i = 0; i < 32; i++) {
             asm.constInt(0x4141414141414141L);
         }
 
+        // sqrt(pi)
+        // asm.ldr(0, "num");
+        // asm.branchLinked("math:sqrt(f64)f64");
+
         asm.ldr(0, "num");
-        asm.branchLinked("math:sqrt");
 
         asm.movAbs(1, "buf");
         asm.mov(2, 256L);
-        asm.vcall(0x4202L); // float.toString
+        asm.vcall(0x4202L); // int.toString
 
         asm.mov(2, 0);
         asm.mov(0, 1L);
---
+++ src/main/java/com/github/sqyyy/urban/assembler/Modules.java
@@ -0,0 +1,67 @@
+package com.github.sqyyy.urban.assembler;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+public final class Modules {
+    public static final AssembledModule MATH;
+
+    static {
+        try {
+            var math = new ModuleAssembler();
+            {
+                // Constants
+                math.labelConst("-math:0.0");
+                math.constFloat(0.0d);
+                math.labelConst("-math:2.0");
+                math.constFloat(2.0d);
+            }
+            {
+                // sqrt(f64): f64
+                math.labelCode("math:sqrt(f64)f64");
+                math.ldr(3, "-math:2.0");
+                math.divf(2, 0, 3);
+                for (int i = 0; i < 16; i++) {
+                    math.divf(1, 0, 2);
+                    math.addf(1, 2, 1);
+                    math.divf(2, 1, 3);
+                }
+                math.mov(0, 2);
+                math.ret();
+                math.panic();
+            }
+            {
+                // abs(i64): i64
+                math.labelCode("math:abs(i64)i64");
+                math.mov(1, 0L);
+                math.cmps(1, 0, 1);
+                math.branchLess(1, "-math:abs(i64)i64#0");
+                math.ret();
+                math.labelCode("-math:abs(i64)i64#0");
+                math.movs(1, 0L);
+                math.subs(0, 1, 0);
+                math.ret();
+                math.panic();
+            }
+            {
+                // abs(f64): f64
+                math.labelCode("math:abs(f64)f64");
+                math.ldr(1, "-math:0.0");
+                math.cmpf(1, 0, 1);
+                math.branchLess(1, "-math:abs(f64)f64#0");
+                math.ret();
+                math.labelCode("-math:abs(f64)f64#0");
+                math.ldr(1, "-math:0.0");
+                math.subf(0, 1, 0);
+                math.ret();
+                math.panic();
+            }
+            MATH = AssembledModule.compile(math);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private Modules() {
+    }
+}
