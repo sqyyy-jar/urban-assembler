73d371e69b847155dc8cc352440b51585c635981
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-03-04 10:15:10 +0100

    Add entrypoint detection

--- src/main/java/com/github/sqyyy/urban/assembler/util/BinaryExporter.java
+++ src/main/java/com/github/sqyyy/urban/assembler/util/BinaryExporter.java
@@ -13,14 +13,19 @@ public class BinaryExporter {
     public static final int EXECUTABLE = 0x1;
     private static final byte[] BYTE_MAGIC = {0, 'u', 'r', 'b'};
 
-    public void exportExecutable(Path binaryPath, Module module, int flags, long entrypointOffset) throws IOException {
+    public void exportExecutable(Path binaryPath, Module module, int flags) throws IOException {
         Files.deleteIfExists(binaryPath);
         Objects.requireNonNull(module);
         try (var file = new RandomAccessFile(binaryPath.toFile(), "rw")) {
             file.write(BYTE_MAGIC);
             Bits.writeInt(file, flags | EXECUTABLE);
+            Bits.writeLong(file, 0L);
+            var entrypointOffset = module.assemble(Channels.newOutputStream(file.getChannel()));
+            if (entrypointOffset == -1) {
+                throw new RuntimeException("Module does not have main function");
+            }
+            file.seek(8);
             Bits.writeLong(file, entrypointOffset);
-            module.assemble(Channels.newOutputStream(file.getChannel()));
         }
     }
 
